ff5b490f94fb65322b48f45f96c587ae9f129b75
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2023-01-05 20:41:15 +0300

    New feature
    Added IStringFormatter Interface

---
+++ StringFormatter/StringFormatter.Core/Interfaces/IStringFormatter.cs
@@ -0,0 +1,7 @@
+﻿namespace StringFormatter.Core.Interfaces;
+
+public interface IStringFormatter
+{
+    // String interpolation for target object
+    string Format(string template, object target);
+}
